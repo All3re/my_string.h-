31593db3a21bdb694e18d4a25c6c749be41e2981
Nikita <146347125+All3re@users.noreply.github.com>
2024-01-30 17:41:04 +0000

    work

--- src/string.c
+++ src/string.c
@@ -14,8 +14,14 @@ int s21_memcmp(const void *str1, const void *str2, size_t n){
     unsigned char* ds = (unsigned char*)str1;
     unsigned char* de = (unsigned char*)str2;
     for(s21_size_t i = 0; i < n; i++){
-        if(ds[i] > de[i]) res = 1;
-        if(ds[i] < de[i]) res = -1;
+        if(ds[i] > de[i]){
+            res = ds[i] - de[i];
+            break;
+        }
+        if(ds[i] < de[i]){
+            res = ds[i] - de[i];
+            break;
+        }
     }
     return res;
 }
@@ -37,4 +43,51 @@ void* s21_memset(void *str, int c, size_t n){
     return str;
 }
 
-char* s21_strncat(char *dest, const char *src, size_t n){}
+s21_size_t s21_strlen(const char *str)
+{
+    s21_size_t len = 0;
+    for(; str[len]; len++);
+    return len + 1;
+} 
+
+
+char* s21_strncat(char *dest, const char *src, size_t n){
+    for(;*dest;){
+        dest++;
+    }
+    for(; n > 0; n--){
+        *dest++ = *src++;
+    }
+    //*de = '\0';
+    return dest;
+}
+
+char* s21_strchr(const char *str, int c){
+    const char* de = s21_NULL;
+    for(;*str;){
+        if(*str == c) {
+            de = str;
+            break;
+        }
+        str++;
+    }
+    return (char*)de;
+}
+
+
+int s21_strncmp(const char *str1, const char *str2, size_t n){
+    int res = 0;
+    for(s21_size_t i = 0; i < n; i++){
+        if(str1[i] > str2[i]){ 
+            res = str1[i] - str2[i];
+            break;
+        }
+        if(str1[i] < str2[i]){
+            res = str1[i] - str2[i];
+            break;
+        }
+    }
+    return res;
+}
+
+char *strncpy(char *dest, const char *src, size_t n){}
